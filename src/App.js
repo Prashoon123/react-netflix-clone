1e26ec392b9ca651962abc7770becd1d29a47b9b
Prashoon123 <64588766+Prashoon123@users.noreply.github.com>
2020-10-13 09:34:24 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import requests from './requests';
 
 function App() {
   return (
-    <div className="App">
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+    // BEM naming convention
+    <div className="app">
+      <Row 
+        title="NETFLIX ORIGINALS" 
+        fetchUrl={requests.fetchNetflixOriginals} 
+        isLargeRow
+      />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      {/* <Row title="Top Rated" />
-      <Row title="Action Movies" /> */}
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -2,22 +2,42 @@ import React, { useState, useEffect } from 'react';
 import axios from './axios';
 import './Row.css';
 
-function Row({ title, fetchUrl }) {
+const base_url = "https://image.tmdb.org/t/p/original/"
+
+function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
 
     // A snippet of code which runs on a specific condition/variable
     useEffect(() => {
         // if [], run once when row loads, and don't run it again
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            console.log(request.data.results);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl]);
 
-    }, []);
+    console.table(movies);
 
     return (
-        <div>
+        // BEM naming convention
+        <div className="row">
             {/* title */}
                 <h2>{title}</h2>
-            {/* container -> posters */}
-            
-            {/*  */}
+                <div className="row__posters">
+                    {/* several row__posters */}
+                    {movies.map(movie => (
+                        <img 
+                            key={movie.id}
+                            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                            // Change this in order to change the card style
+                            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
+                            alt={movie.name}
+                        />
+                    ))}
+                </div>
         </div>
     )
 }
