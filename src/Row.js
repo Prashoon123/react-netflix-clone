30e0078f9a58c4b18a38fecb1d8defec3b6808f1
Prashoon123 <64588766+Prashoon123@users.noreply.github.com>
2020-10-13 20:21:02 +0530

    Add Trailer popups DONE✅

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 import './Row.css';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 
 const base_url = "https://image.tmdb.org/t/p/original/"
 
 function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
 
     // A snippet of code which runs on a specific condition/variable
     useEffect(() => {
@@ -18,6 +21,28 @@ function Row({ title, fetchUrl, isLargeRow }) {
         fetchData();
     }, [fetchUrl]);
 
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            // https://developers.google.com/youtube/player_parameters,
+            autoplay: 1,
+        },
+    };
+
+    const handleClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl('');
+        } else {
+            movieTrailer(movie?.name || "")
+            .then(url => {
+                // https://youtu.be/LVOQdltQjAQ
+                const urlParams = new URLSearchParams(new URL(url).search)
+                setTrailerUrl(urlParams.get('v'));
+            }).catch(error => console.log(error));
+        }
+    };
+
     return (
         // BEM naming convention
         <div className="row">
@@ -27,6 +52,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
                     {/* several row__posters */}
                     {movies.map(movie => (
                         <img 
+                            onClick={() => handleClick(movie)}
                             key={movie.id}
                             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
                             // Change this in order to change the card style
@@ -35,6 +61,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
                         />
                     ))}
                 </div>
+                {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
     )
 }
